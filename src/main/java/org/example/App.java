04ffd1aebae736dfccfe5afc3999f6f8e8262302
Filmon-coder <eaglefifi@gmail.com>
2021-01-17 19:23:30 -0500

    HelloWorld JAVA

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+        System.out.println("Author: Filmon Abraham");
+        System.out.println("Class: ITN264");
+        System.out.println("Date: 16/01/2021");
+        System.out.println("Assignment:Assignment 1");
+        System.out.println("My First Assignmet");
+    }
+}
